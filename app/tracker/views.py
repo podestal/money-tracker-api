69a4e8970e38cc797f487794f7ea76d87f0c5ce1
podestal <luisrodriguezpodesta@gmail.com>
2024-09-19 12:15:20 -0400

    filtering transactions by month and year

--- app/tracker/views.py
+++ app/tracker/views.py
@@ -5,7 +5,11 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import status
+
+from django.utils.timezone import make_aware
+from datetime import datetime
+import calendar
+
 from . import models
 from . import serializers
 
@@ -50,8 +54,37 @@ class TransactionViewSet(ModelViewSet):
     serializer_class = serializers.TransactionSerializer
     permission_classes = [permissions.IsAuthenticated]
     
+    
     def get_queryset(self):
-        """Retrieves filtered transactions for authenticated users, and all for superuser"""
-        if self.request.user.is_superuser:
-            return models.Transaction.objects.select_related('user', 'category').order_by('-created_at')
-        return models.Transaction.objects.filter(user=self.user).select_related('user', 'category').order_by('-created_at')
+        """ Retrieves filtered transactions for authenticated users, and all for superuser """
+        queryset = models.Transaction.objects.select_related('user', 'category').order_by('-id')
+
+        # If the current user is not a superuser, filter transactions for only the logged-in user
+        if not self.request.user.is_superuser:
+            queryset = queryset.filter(user=self.user)
+
+        # Extract 'created_at' from the request's query parameters
+        created_at = self.request.query_params.get('created_at')
+
+        if created_at:
+            try:
+                # Try parsing the 'created_at' parameter as a date in the format "YYYY-MM-DD"
+                date = datetime.strptime(created_at, "%Y-%m-%d")
+                
+                # Get the first day of the month (e.g., "2024-09-01" for September 2024)
+                first_day = make_aware(datetime(date.year, date.month, 1))
+                
+                # Get the last day of the month by determining the total number of days in that month
+                last_day = make_aware(datetime(date.year, date.month, calendar.monthrange(date.year, date.month)[1]))
+                
+                # Filter the queryset by the range from the first day to the last day of the specified month
+                queryset = queryset.filter(created_at__range=(first_day, last_day))
+
+            # Handle any errors caused by invalid date formats
+            except ValueError as e:
+                # Log the error and return an empty queryset if the date is invalid
+                print('Value Error', e)
+                queryset = queryset.none()
+
+        # Return the filtered queryset
+        return queryset
