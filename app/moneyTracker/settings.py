1fddcf3b0dbaac9c465703c4c1ededfb337cf544
podestal <luisrodriguezpodesta@gmail.com>
2024-09-24 13:35:31 -0400

    nginx bug fixed

--- app/moneyTracker/settings.py
+++ app/moneyTracker/settings.py
@@ -22,19 +22,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#sm5&7lx6gfgp0c$c&rn&uoq@g-ub5yq6nv3vx3)dhvph)-1s#'
-# SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
+# SECRET_KEY = 'django-insecure-#sm5&7lx6gfgp0c$c&rn&uoq@g-ub5yq6nv3vx3)dhvph)-1s#'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-# ALLOWED_HOSTS.extend(
-#     filter(
-#         None,
-#         os.environ.get('ALLOWED_HOSTS', '').split(','),
-#     )
-# )
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
@@ -161,5 +161,3 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
