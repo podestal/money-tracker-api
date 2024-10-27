ba5db120013253c702842b11e6908abef488e055
podestal <luisrodriguezpodesta@gmail.com>
2024-10-27 11:21:41 -0400

    filtering users by email

--- app/core/views.py
+++ app/core/views.py
@@ -1,9 +1,8 @@
 from djoser.views import UserViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from . import models
 
 
 class FilteredUserViewSet(UserViewSet):
 
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["username"]
+    filterset_fields = ["username", "email"]
